7d4888d89609d40ce2fe61abb8577f34233736a9
ilevd <ilevd@mail.ru>
2018-02-28 12:54:39 +0700

    Change version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.anychart/link-checker "0.2.5"
+(defproject com.anychart/link-checker "0.2.6"
   :description "Library for finding broken links"
   :url "https://github.com/AnyChart/link-checker"
   :license {:name "MIT License"
